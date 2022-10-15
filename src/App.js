4cc5da4a42234054b9a21649457e93bdecc76c55
ddessilvestri <silvestri.des@gmail.com>
2022-10-15 20:16:17 -0300

    products array created and map function implemented

--- src/App.js
+++ src/App.js
@@ -1,6 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
 
+const products = [
+  {
+    id:1,
+    name:'Product 1',
+    price: 100,
+    decription: 'Product 1 Description'
+  },
+  {
+    id:2,
+    name:'Product 2',
+    price: 150,
+    decription: 'Product 2 Description'
+  },
+  {
+    id:3,
+    name:'Product 3',
+    price: 60,
+    decription: 'Product 3 Description'
+  },
+]
+
 function App() {
   const onHandleClick = ()=>{
     console.log("click");
@@ -17,6 +38,14 @@ function App() {
           className='button-primary'
           >
             Click me</button>
+
+            {products.map((product)=>(
+              <div key={product.id}>
+                <h3>{product.name}</h3>
+                <p>{product.decription}</p>
+              </div>
+
+            ))}
       </header>
     </div>
   );
